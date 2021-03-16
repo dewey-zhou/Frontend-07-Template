b6b389b7b82505bfb68757d865ec09d1ba0b18cd
dewey <1317336077@qq.com>
2021-03-16 17:24:36 +0800

    14th over

--- Week14/JSX/main.js
+++ Week14/JSX/main.js
@@ -16,30 +16,63 @@ class Carousel extends Component{
             child.style.backgroundImage = `url('${record}')`
             this.root.appendChild(child)
         }
+        //自动轮播模块
+        // let currentIndex = 0
+        // setInterval(()=>{
+        //     let children = this.root.children
+        //     let nextIndex = (currentIndex +1)%children.length
+        //     let current = children[currentIndex]
+        //     let next = children[nextIndex]
 
-        let currentIndex = 0
-        setInterval(()=>{
+        //     next.style.transition = "none"
+        //     next.style.transform = `translateX(${100-nextIndex*100}%)`
+        //     setTimeout(()=>{
+        //         next.style.transition = ""
+        //         current.style.transform=`translateX(${-100-currentIndex*100}%)`
+        //         next.style.transform=`translateX(${-nextIndex*100}%)`
+        //         currentIndex = nextIndex
+        //     },16)
+        // },3000)
+
+        // 鼠标控制轮播
+        let position = 0
+        this.root.addEventListener('mousedown',event=>{
             let children = this.root.children
-            let nextIndex = (currentIndex +1)%children.length
-            let current = children[currentIndex]
-            let next = children[nextIndex]
+            let startX = event.clientX
+            let move = event =>{
+                let x = event.clientX-startX
 
-            next.style.transition = "none"
-            next.style.transform = `translateX(${100-nextIndex*100}%)`
-            setTimeout(()=>{
-                next.style.transition = ""
-                current.style.transform=`translateX(${-100-currentIndex*100}%)`
-                next.style.transform=`translateX(${-nextIndex*100}%)`
-                currentIndex = nextIndex
-            },16)
-        },3000)
+                let current =position -((x-x%500)/500)
+                for(let offset of [-1,0,1]){
+                    let pos = current+offset
+                    pos = (pos +children.length)%children.length
+                    children[pos].style.transition = "none"
+                    children[pos].style.transform = `translateX(${-pos *500+offset * 500 + x}px)`
+                }
+            }
+            let up = event=>{
+               let x = event.clientX-startX
+               position = position -Math.round(x/500)
+               for(let offset of [0,-Math.sign(Math.round(x/500)-x+250*Math.sign(x))]){
+                let pos = position+offset
+                    pos = (pos +children.length)%children.length
+                children[pos].style.transition = ""
+                
+                children[pos].style.transform = `translateX(${-pos *500 +offset * 500 }px)`
+                }
+                document.removeEventListener("mousemove",move)
+                document.removeEventListener("mouseup",up)
+            }
+            document.addEventListener('mousemove',move)
+            document.addEventListener('mouseup',up)
+        })
         return  this.root
     }
     mountTo(parent){
         parent.appendChild(this.render())
     }
 }
-console.log('test')
+console.log('test2')
 var d = [
     "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
     "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
